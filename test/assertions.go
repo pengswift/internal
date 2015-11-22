cdb89b5a551c60f9a85bd2055884d23b302618de
pengswift <peng.swift+coder@gmail.com>
2015-11-22 10:25:03 +0800

    fix

--- test/assertions.go
+++ test/assertions.go
@@ -7,20 +7,29 @@ import (
 	"testing"
 )
 
-func Equal(t *testing.T, expected, actual interface{}) {
-	if !reflect.DeepEqual(expected, actual) {
+func Assert(t *testing.T, condition bool, msg string, v ...interface{}) {
+	if !condition {
 		_, file, line, _ := runtime.Caller(1)
-		t.Logf("\033[31m%s:%d:\n\n\t   %#v (expected)\n\n\t!= %#v (actual)\033[39m\n\n",
-			filepath.Base(file), line, expected, actual)
+		t.Logf("\033[31m%s:%d: "+msg+"\033[39m\n\n",
+			append([]interface{}{filepath.Base(file), line}, v...)...)
 		t.FailNow()
 	}
 }
 
-func NotEqual(t *testing.T, expected, actual interface{}) {
-	if !reflect.DeepEqual(expected, actual) {
+func Equal(t *testing.T, act, exp interface{}) {
+	if !reflect.DeepEqual(exp, act) {
 		_, file, line, _ := runtime.Caller(1)
-		t.Logf("\033[31m%s:%d:\n\n\tvalue should not equal %#v\033[39m\n\n",
-			filepath.Base(file), line, actual)
+		t.Logf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n",
+			filepath.Base(file), line, exp, act)
+		t.FailNow()
+	}
+}
+
+func NotEqual(t *testing.T, act, exp interface{}) {
+	if reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		t.Logf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n",
+			filepath.Base(file), line, exp, act)
 		t.FailNow()
 	}
 }
